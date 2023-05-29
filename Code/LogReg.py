7641b0b22a5f78504e99924615bb8b506e464862
anlanter <102613946+anlanter@users.noreply.github.com>
2023-05-29 21:44:46 +0200

    linreg.py .

--- Code/LogReg.py
+++ Code/LogReg.py
@@ -15,16 +15,14 @@ y = rawData.Outcome
 #Data splitting
 X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.2,random_state=0)
 
+#logReg
 logistic = LogisticRegression()
 logistic.fit(X_train,y_train)
-
 accuracy = logistic.score(X_test,y_test)
-print("Accurecy = " , accuracy * 100, "%")
-
 log_prediciton = logistic.predict(X_test)
-
-print("Classification Report is:\n", classification_report(y_test, log_prediciton))
+print("Accuracy = " , accuracy * 100, "%")
+print("Clas. Report:", classification_report(y_test, log_prediciton))
 print("Confusion Matrix:\n", confusion_matrix(y_test, log_prediciton))
-print("Training Score:\n", logistic.score(X_train, y_train) * 100)
-print("Mean Squared Error:\n", mean_squared_error(y_test, log_prediciton))
-print("R2 score is:\n", r2_score(y_test, log_prediciton))
+print("Training Score:", logistic.score(X_train, y_train) * 100)
+print("Mean Squared Error:", mean_squared_error(y_test, log_prediciton))
+print("R2 score is:", r2_score(y_test, log_prediciton))
