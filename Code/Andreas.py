7babb79ae43a2a648ffc357f556def6fa3684863
anlanter <102613946+anlanter@users.noreply.github.com>
2023-05-29 15:47:08 +0200

    ok

--- Code/Andreas.py
+++ Code/Andreas.py
@@ -1,12 +1,13 @@
 import pandas as pd
-import numpy as np
+"""import numpy as np
 import matplotlib.pyplot as plt
 from sklearn import svm
 from scipy import stats
 import seaborn as sns
-import os
+import os"""
 
 
 # loading the data :) --gf
-rawData = pd.read_csv('../data/diabetes.csv')
+rawData = pd.read_csv('../data/clean_data.csv')
+
 
