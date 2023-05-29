410d629b904c312a510d92f117930ed5a974a915
andreaslanter <anlanter@student.ethz.ch>
2023-05-29 14:57:30 +0200

    check

--- Code/Andreas.py
+++ Code/Andreas.py
@@ -1,1 +1,12 @@
-#Hallo Andi
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn import svm
+from scipy import stats
+import seaborn as sns
+import os
+
+
+# loading the data :)
+rawData = pd.read_csv('../data/diabetes.csv')
+
