b35afcd999b8a6757fb7ea794c652781dffd2d94
TropfenderWasserhahn <jensbo@ethz.ch>
2023-06-10 11:35:33 +0200

    main skeleton erstellt

---
+++ Code/main.py
@@ -0,0 +1,26 @@
+# installs and imports with version
+# asdf
+
+# load the data
+# data=...
+
+# data exploration
+# head, shape, columns, info, describe, plots(hist, box, correlation), ...
+
+# data cleaning
+# replace the zeros with NaN --> new plots
+
+# data imputation / data normalization --> what first?
+# impute the missing values with / normalize the data --> probably both necessary
+
+# train-test-split
+# normal distribution 80-20
+
+# ML models
+# LogReg, SVM, DecisionTree, RandomForrest
+
+# results
+# confusion matrix, accuracy, F1.score, ROC
+
+# outlook
+# what else could / should be done
