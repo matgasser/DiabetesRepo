5931b9a8cd0065404131c5c62cb976363000810a
TropfenderWasserhahn <jensbo@ethz.ch>
2023-06-10 12:42:54 +0200

    main skeleton mit daten input, plots und cleaning

--- Code/main.py
+++ Code/main.py
@@ -1,26 +1,113 @@
-# installs and imports with version
-# asdf
+" installs and imports with version "
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import os
+import matplotlib.pyplot as plt
+# import plotly.graph_objects as go
+# from plotly.subplots import make_subplots
+from sklearn.cluster import KMeans
+from scipy.spatial.distance import cdist
+from sklearn.metrics import silhouette_score, adjusted_rand_score
+from scipy.stats import ranksums, spearmanr, pearsonr
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import StandardScaler, LabelEncoder
+from sklearn.cluster import DBSCAN
+from sklearn.metrics import roc_curve, roc_auc_score
+from sklearn.metrics import roc_curve, confusion_matrix, auc
+from sklearn.model_selection import StratifiedKFold
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
 
-# load the data
-# data=...
+import warnings
+warnings.filterwarnings("ignore")
+
+" load the data "
+rawData = pd.read_csv('../data/diabetes.csv')
 
 # data exploration
-# head, shape, columns, info, describe, plots(hist, box, correlation), ...
+def data_exploration(data):
+    print(data.head(10))
+    print(data.shape)
+    print(data.columns)
+    print(data.info())
+    print(data.describe())
+    print(data.isna().sum())
 
-# data cleaning
-# replace the zeros with NaN --> new plots
+data_exploration(rawData)
 
-# data imputation / data normalization --> what first?
+def create_histplots (csv_file, output_folder):
+    data = pd.read_csv(csv_file)
+    output_folder = os.path.abspath(output_folder)
+    fig, axes = plt.subplots(3, 3, figsize=(15, 18))
+
+    for i, column in enumerate(data.columns):
+        row = i // 3
+        col = i % 3
+        ax = axes[row, col]
+        ax.hist(data[column], bins='auto', edgecolor='black', linewidth=1.2)
+
+        ax.set_title(f"Distribution of raw data: {column}")
+        ax.set_xlabel(column)
+        ax.set_ylabel("Count")
+
+        ax.grid(True)
+
+        output_file = os.path.join(output_folder, "histplots_raw_Data.png")
+    plt.savefig(output_file)
+
+csv_file = "../data/diabetes.csv"
+output_folder = "../Output"
+
+create_histplots(csv_file, output_folder)
+
+" data cleaning "
+clean_Data = rawData.copy(deep = True)
+clean_Data[['Glucose', 'BloodPressure', 'SkinThickness', 'Insulin', 'BMI']] = clean_Data[['Glucose', 'BloodPressure', 'SkinThickness', 'Insulin', 'BMI']].replace(0, np.NaN)
+
+# save the clean data
+clean_Data.to_csv("../Data/clean_Data.csv", index = False)
+
+data_exploration(clean_Data)
+
+def create_histplots (csv_file, output_folder):
+    data = pd.read_csv(csv_file)
+    output_folder = os.path.abspath(output_folder)
+    fig, axes = plt.subplots(3, 3, figsize=(15, 18))
+
+    for i, column in enumerate(data.columns):
+        row = i // 3
+        col = i % 3
+        ax = axes[row, col]
+        ax.hist(data[column], bins='auto', edgecolor='black', linewidth=1.2)
+
+        ax.set_title(f"Distribution of clean data: {column}")
+        ax.set_xlabel(column)
+        ax.set_ylabel("Count")
+
+        ax.grid(True)
+
+
+        output_file = os.path.join(output_folder, "histplots_clean_Data.png")
+    plt.savefig(output_file)
+
+csv_file = "../data/clean_Data.csv"
+output_folder = "../Output"
+
+" data imputation / data normalization --> what first? "
 # impute the missing values with / normalize the data --> probably both necessary
 
-# train-test-split
+" train-test-split "
 # normal distribution 80-20
 
-# ML models
+" ML models "
 # LogReg, SVM, DecisionTree, RandomForrest
 
-# results
+" results "
 # confusion matrix, accuracy, F1.score, ROC
 
-# outlook
+" outlook "
 # what else could / should be done
